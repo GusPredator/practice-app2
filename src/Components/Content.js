896ed6e02cd10913efd892c96c0274795317091e
Gustavo Sánchez <gustavo.sanchez@korinver.com>
2018-12-02 11:55:30 -0600

    Test commit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -8,21 +8,23 @@ class Content extends Component {
     render() {
         return (
             <div className="mainContainer">
-                <Carousel
-                    width='500px'
-                    dynamicHeight={true}
-                    centerMode={true}
-                >
-                    <div>
-                        <img src="https://images5.alphacoders.com/802/802067.jpg" alt="diana" />
-                    </div>
-                    <div>
-                        <img src="http://lolwp.com/wp-content/uploads/2012/02/Arcade-Skins-Wallpaper.jpg" alt="arcadia" />
-                    </div>
-                    <div>
-                        <img src="https://images6.alphacoders.com/651/651136.jpg" alt="kindred" />
-                    </div>
-                </Carousel>
+                <div className="carouselContainer">
+                    <Carousel
+                        className="carouselContainer"
+                        dynamicHeight={true}
+                    >
+                        <div>
+                            <img src="https://images5.alphacoders.com/802/802067.jpg" alt="diana" />
+                        </div>
+                        <div>
+                            <img src="http://lolwp.com/wp-content/uploads/2012/02/Arcade-Skins-Wallpaper.jpg" alt="arcadia" />
+                        </div>
+                        <div>
+                            <img src="https://images6.alphacoders.com/651/651136.jpg" alt="kindred" />
+                        </div>
+                    </Carousel>
+                </div>
+                
                 <div className="sectionContainer">
                     <div className="sectionOne">
                         Hola
